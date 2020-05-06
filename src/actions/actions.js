02dada2564fbf76c03337e738d86d189fc2b791b
Kevin Åberg Kultalahti <kevin@kevmodro.me>
2020-05-06 18:11:47 +0200

    fixes test when update but not destroy is supplied by action

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -5,7 +5,7 @@ export function useActions(node, actions = []) {
   actions.forEach(([action, options]) => {
 
     // Save the destroy method, supply a dummy one if the action doesn't contain one.
-    const { destroy } = action(node, options) || { destroy: () => { } }
+    const { destroy = () => { } } = action(node, options) || { destroy: () => { } }
     cleanUpFunctions.push(destroy)
   })
 
